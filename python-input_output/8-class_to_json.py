a3f3277ac5470854d59d7334e485b23f8052104a
Amelie-Manzi <a.gatego@alustudent.com>
2025-03-14 14:40:30 -0700

    updated

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -4,16 +4,4 @@
 
 def class_to_json(obj):
     """return dictionary description for json serialization of an object"""
-    return vars(obj)i#!/usr/bin/python3
-"""define function class_to_json(obj)"""
-
-
-def class_to_json(obj):
-    """return dictionary description for json serialization of an object"""
-    return vars(obj)#!/usr/bin/python3
-"""define function class_to_json(obj)"""
-
-
-def class_to_json(obj):
-    """return dictionary description for json serialization of an object"""
-    return vars(obj)iujgg
+    return vars(obj)
